2cd4deacc296d472457ddba9abbd75d695408ad9
shanyiyuan <shanyiyuan@csweep.com>
2018-05-16 15:08:36 +0800

    一些文件

---
+++ IPA/STRIAPManager.h
@@ -0,0 +1,27 @@
+//
+//  STRIAPManager.h
+//  KidEducation
+//
+//  Created by cceup on 2018/5/16.
+//  Copyright © 2018年 zz77. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    SIAPPurchSuccess = 0,       // 购买成功
+    SIAPPurchFailed = 1,        // 购买失败
+    SIAPPurchCancle = 2,        // 取消购买
+    SIAPPurchVerFailed = 3,     // 订单校验失败
+    SIAPPurchVerSuccess = 4,    // 订单校验成功
+    SIAPPurchNotArrow = 5,      // 不允许内购
+}SIAPPurchType;
+
+typedef void (^IAPCompletionHandle)(SIAPPurchType type,NSData *data);
+
+
+@interface STRIAPManager : NSObject
++ (instancetype)shareSIAPManager;
+//开始内购
+- (void)startPurchWithID:(NSString *)purchID completeHandle:(IAPCompletionHandle)handle;
+@end
